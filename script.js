d531b851c45b493c5f3ea04d38e41aaf3600a62c
IsraelFelipe1997 <92111905+IsraelFelipe1997@users.noreply.github.com>
2024-02-29 17:42:05 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,71 +12,72 @@ let dataframe = []; // Array para armazenar os dados
 
 customKeys.forEach(key => {
   key.addEventListener('click', () => {
-    answerInput.value += key.textContent;})
+    answerInput.value += key.textContent;
+  });
 });
 
 zeroButton.addEventListener('click', () => {
-  answerInput.value += '0';});
+  answerInput.value += '0';
+});
 
-  clearButton.addEventListener('click', () => {
-    answerInput.value = null;
+clearButton.addEventListener('click', () => {
+  answerInput.value = null;
 });
 
 function generateQuestion() {
-    const randomTabuada = selectedTabuadas[Math.floor(Math.random() * selectedTabuadas.length)];
-    const num2 = Math.floor(Math.random() * 10) + 1; // Número aleatório de 1 a 10
-    currentAnswer = randomTabuada * num2; // Armazena a resposta atual
-    questionContainer.innerHTML = `<br><span class="question-math">Pergunta:<br>${randomTabuada} x ${num2} = ?</span>`;
+  const randomTabuada = selectedTabuadas[Math.floor(Math.random() * selectedTabuadas.length)];
+  const num2 = Math.floor(Math.random() * 10) + 1; // Número aleatório de 1 a 10
+  currentAnswer = randomTabuada + num2; // Alterado para adição
+  questionContainer.innerHTML = `<br><span class="question-math">Pergunta:<br>${randomTabuada} + ${num2} = ?</span>`; // Alterado para adição
 }
 
 function startChallenge() {
-    selectedTabuadas = []; // Limpa as tabuadas selecionadas
-    checkboxes.forEach(checkbox => {
-        if (checkbox.checked) {
-            selectedTabuadas.push(parseInt(checkbox.value));
-        }
-    });
-    if (selectedTabuadas.length > 0) {
-        generateQuestion(); // Gera a primeira pergunta ao iniciar o desafio
-    } else {
-        alert('Thawane❤️, você deve selecionar pelo menos uma tabuada para estudar.');
+  selectedTabuadas = []; // Limpa as tabuadas selecionadas
+  checkboxes.forEach(checkbox => {
+    if (checkbox.checked) {
+      selectedTabuadas.push(parseInt(checkbox.value));
     }
+  });
+  if (selectedTabuadas.length > 0) {
+    generateQuestion(); // Gera a primeira pergunta ao iniciar o desafio
+  } else {
+    alert('Thawane❤️, você deve selecionar pelo menos uma tabuada para estudar.');
+  }
 }
 
 function tocarSomAcerto() {
-    const audioAcerto = document.getElementById('audio-acerto');
-    audioAcerto.play();
+  const audioAcerto = document.getElementById('audio-acerto');
+  audioAcerto.play();
 }
 
 function tocarSomErro() {
-    const audioErro = document.getElementById('audio-erro');
-    audioErro.play();
+  const audioErro = document.getElementById('audio-erro');
+  audioErro.play();
 }
 
 function checkAnswer() {
-    const userAnswer = parseInt(answerInput.value);
-    const status = userAnswer === currentAnswer ? 'Acerto' : 'Erro';
-    const currentDate = new Date().toLocaleString(); // Obtém a data e hora atuais
+  const userAnswer = parseInt(answerInput.value);
+  const status = userAnswer === currentAnswer ? 'Acerto' : 'Erro';
+  const currentDate = new Date().toLocaleString(); // Obtém a data e hora atuais
 
-    dataframe.push({
-        data: currentDate,
-        pergunta: questionContainer.textContent,
-        resposta: userAnswer,
-        status: status
-    });
+  dataframe.push({
+    data: currentDate,
+    pergunta: questionContainer.textContent,
+    resposta: userAnswer,
+    status: status
+  });
 
-    if (userAnswer === currentAnswer) {
-        score++;
-        resultContainer.innerHTML  = '<span class="result-green">Correto!❤️</span>';
-        tocarSomAcerto(); // Chama a função para reproduzir o som de acerto
-    } else {
-        resultContainer.innerHTML  = `<span class="result-red">Incorreto!😠 A resposta correta é ${currentAnswer}.</span>`;
-        tocarSomErro(); // Chama a função para reproduzir o som de erro
-    }
-    answerInput.value = ''; // Limpa o campo de resposta
-    setTimeout(() => {
-        resultContainer.textContent = ''; // Limpa a mensagem após alguns segundos
-        generateQuestion(); // Gera uma nova pergunta após verificar a resposta
-    }, 3000); // 3000 milissegundos = 3 segundos
+  if (userAnswer === currentAnswer) {
+    score++;
+    resultContainer.innerHTML  = '<span class="result-green">Correto!❤️</span>';
+    tocarSomAcerto(); // Chama a função para reproduzir o som de acerto
+  } else {
+    resultContainer.innerHTML  = `<span class="result-red">Incorreto!😠 A resposta correta é ${currentAnswer}.</span>`;
+    tocarSomErro(); // Chama a função para reproduzir o som de erro
+  }
+  answerInput.value = ''; // Limpa o campo de resposta
+  setTimeout(() => {
+    resultContainer.textContent = ''; // Limpa a mensagem após alguns segundos
+    generateQuestion(); // Gera uma nova pergunta após verificar a resposta
+  }, 3000); // 3000 milissegundos = 3 segundos
 }
-
